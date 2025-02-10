f39e26c27f428d73172d42e36aa4718ea54d1300
NicolasGSaid <nicolasgithub2@gmail.com>
2025-02-10 17:22:39 -0300

    nem sei oq eu fiz

--- angular10/src/app/shared.service.ts
+++ angular10/src/app/shared.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class SharedService {
 readonly APIUrl = "http://127.0.0.1:8000/";
-readonly PhotoUrl = "http://127.0.0.1:8000/media/";
+
 
   constructor(private http:HttpClient) { }
 
@@ -25,28 +25,5 @@ readonly PhotoUrl = "http://127.0.0.1:8000/media/";
 
   deleteTarefa(val:any){
     return this.http.delete(this.APIUrl + 'tarefa/',val);
-  }
-  getUsuarioList():Observable<any[]>{
-    return this.http.get<any>(this.APIUrl + 'usuario/');
-  }
-
-  addUsuario(val:any){
-    return this.http.post(this.APIUrl + 'usuario/',val);
-  }
-  
-  updateUsuario(val:any){
-    return this.http.put(this.APIUrl + 'usuario/',val);
-  }
-
-  deleteUsuario(val:any){
-    return this.http.delete(this.APIUrl + 'usuario/',val);
-  }
-
-  UploadPhoto(val:any){
-    return this.http.post(this.APIUrl + 'SaveFile',val);
-  }
-
-  getAllTarefaNames():Observable<any[]>{
-    return this.http.get<any>(this.APIUrl + 'tarefa/');
   }
 }
